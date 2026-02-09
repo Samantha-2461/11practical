7eb8e82bebc66bc72461df2d2d0e87002f95803e
Samantha-2461 <siwelasamantha49@gmail.com>
2026-02-09 16:20:07 +0200

    Update 11pratical.java

--- 11pratical.java
+++ 11pratical.java
@@ -1,19 +1,19 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class Main {
+public class 11practical {
     public static void main(String[] args) {
-        int N = 5; // Fixed: N must be defined
+        int N = 5; 
         int[] shuffled = new int[N];
 
-        // Fixed: Simply filling the array for demonstration
+        
         for (int b = 0; b < N; b++) {
             shuffled[b] = b + 1;
         }
 
-        // Fixed: D must be declared and instantiated
+        
         Map<String, Integer> D = new HashMap<>();
-        String key1 = "132"; // Renamed to avoid shadowing
+        String key1 = "132"; 
 
         if (!D.containsKey(key1)) {
             D.put(key1, 1);
@@ -21,7 +21,7 @@ public class Main {
             D.put(key1, D.get(key1) + 1);
         }
 
-        // Output results
+        
         for (String key : D.keySet()) {
             System.out.println(key + ", " + D.get(key));
         }
